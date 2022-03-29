cf59548dff9e604a46b2eabac140de9b859cadc5
A_ZETA_A <56914556+ZETA-A@users.noreply.github.com>
2022-03-29 22:09:49 +0900

    완성

---
+++ js/invite.js
@@ -0,0 +1,7 @@
+function sefirot() {
+    location.href='https://discord.com/oauth2/authorize?client_id=939969671506055208&permissions=414464822336&scope=applications.commands%20bot';
+};
+
+function pia() {
+    window.alert('아직 개발 중 입니다! 곧 돌아올게요!');
+};
